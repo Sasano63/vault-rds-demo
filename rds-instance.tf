57fa230282479c9848fd22ce388c198f73e50f03
Sasano63 <sasano@hashicorp.com>
2022-10-14 17:37:06 +0200

    w/o hcp

--- rds-instance.tf
+++ rds-instance.tf
@@ -1,41 +1,41 @@
 
-data "aws_availability_zones" "available" {
-  state = "available"
-}
+# data "aws_availability_zones" "available" {
+#   state = "available"
+# }
 
-resource "aws_subnet" "subnet1" {
-  vpc_id     = data.terraform_remote_state.hcp.outputs.vpc
-  cidr_block = var.subnet-block1
-  availability_zone = data.aws_availability_zones.available.names[0]
+# resource "aws_subnet" "subnet1" {
+#   vpc_id     = data.terraform_remote_state.hcp.outputs.vpc
+#   cidr_block = var.subnet-block1
+#   availability_zone = data.aws_availability_zones.available.names[0]
 
-  tags = {
-    Name = "db-subnet-1"
-  }
-}
+#   tags = {
+#     Name = "db-subnet-1"
+#   }
+# }
 
-resource "aws_subnet" "subnet2" {
-  vpc_id     = data.terraform_remote_state.hcp.outputs.vpc
-  cidr_block = var.subnet-block2
-  availability_zone = data.aws_availability_zones.available.names[1]
+# resource "aws_subnet" "subnet2" {
+#   vpc_id     = data.terraform_remote_state.hcp.outputs.vpc
+#   cidr_block = var.subnet-block2
+#   availability_zone = data.aws_availability_zones.available.names[1]
 
-  tags = {
-    Name = "db-subnet-2"
-  }
-}
+#   tags = {
+#     Name = "db-subnet-2"
+#   }
+# }
 
-resource "aws_route_table_association" "rtb_subnet1" {
-  subnet_id      = aws_subnet.subnet1.id
-  route_table_id = data.terraform_remote_state.hcp.outputs.rtb
-}
+# resource "aws_route_table_association" "rtb_subnet1" {
+#   subnet_id      = aws_subnet.subnet1.id
+#   route_table_id = data.terraform_remote_state.hcp.outputs.rtb
+# }
 
-resource "aws_route_table_association" "rtb_subnet2" {
-  subnet_id      = aws_subnet.subnet2.id
-  route_table_id = data.terraform_remote_state.hcp.outputs.rtb
-}
+# resource "aws_route_table_association" "rtb_subnet2" {
+#   subnet_id      = aws_subnet.subnet2.id
+#   route_table_id = data.terraform_remote_state.hcp.outputs.rtb
+# }
 
 resource "aws_db_subnet_group" "db-subnetgroup" {
   name       = "dbsubnetgroup"
-  subnet_ids = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  subnet_ids = [data.terraform_remote_state.vault.outputs.subnet1, data.terraform_remote_state.vault.outputs.subnet2, data.terraform_remote_state.vault.outputs.subnet3]
 
   tags = {
     Name = "Vault DB subnet group"
