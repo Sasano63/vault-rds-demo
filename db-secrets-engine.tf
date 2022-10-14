b86da14b78bc6b4aeec1e089c4d06a6edc9b42fa
Sasano63 <sasano@hashicorp.com>
2022-10-14 15:30:31 +0200

    db engine config

--- db-secrets-engine.tf
+++ db-secrets-engine.tf
@@ -1,13 +1,36 @@
-# resource "vault_database_secrets_mount" "db" {
-#   path = "db"
 
-#   mssql {
-#     name           = "db1"
-#     username       = "sa"
-#     password       = "super_secret_1"
-#     connection_url = "sqlserver://{{username}}:{{password}}@127.0.0.1:1433"
-#     allowed_roles = [
-#       "dev1",
-#     ]
-#   }
-# }
+
+resource "vault_mount" "db" {
+  path = "mysql"
+  type = "database"
+}
+
+resource "vault_database_secret_backend_connection" "mysql" {
+  backend       = vault_mount.db.path
+  name          = "postgres"
+  allowed_roles = ["demo-long", "demo-short"]
+
+  mysql {
+    username       = var.username
+    password       = var.dbpassword
+    connection_url = aws_db_instance.rds-db.address
+    }
+}
+
+resource "vault_database_secret_backend_role" "role-long" {
+  backend             = vault_mount.db.path
+  name                = "demo-long"
+  db_name             = vault_database_secret_backend_connection.mysql.name
+  creation_statements = "CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL ON my_app.* TO '{{name}}'@'%';"
+  default_ttl         = "1h" 
+  max_ttl             = "24h"
+}
+
+resource "vault_database_secret_backend_role" "role-short" {
+  backend             = vault_mount.db.path
+  name                = "demo-short"
+  db_name             = vault_database_secret_backend_connection.mysql.name
+  creation_statements = "CREATE USER '{{name}}'@'%' IDENTIFIED BY '{{password}}';GRANT ALL ON my_app.* TO '{{name}}'@'%';"
+  default_ttl         = "3m" 
+  max_ttl             = "10m"
+}
